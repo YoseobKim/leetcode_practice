7ce7e2402643c2cea79c396cf0f512083b92ca2c
Yoseob Kim <yo1226@gmail.com>
2019-04-07 22:14:11 +0900

    Create 70_Climbing_Stairs_YS_KIM_2019_04_07.java

---
+++ 70/70_Climbing_Stairs_YS_KIM_2019_04_07.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int climbStairs(int n) {
+        if(n <= 0) return 0;
+        if(n <= 1) return 1;
+        if(n <= 2) return 2;
+        
+        int[] arr = new int[n + 1];
+        arr[0] = 0;
+        arr[1] = 1;
+        arr[2] = 2;
+        
+        for(int i = 3; i < arr.length; i++) {
+            arr[i] = arr[i - 2] + arr[i - 1];
+        }
+        
+        return arr[n];
+    }
+}
