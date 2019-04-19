b2627209dfdc4735a241cc988aaeaf0ebfa5e69b
Yoseob Kim <yo1226@gmail.com>
2019-04-20 00:15:09 +0900

    Create 136_Single_Number_YS_KIM_2019_04_20.java

---
+++ 130/136_Single_Number_YS_KIM_2019_04_20.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        Set<Integer> entire = new HashSet<>();
+        Set<Integer> duplicated = new HashSet<>();
+        
+        for(int i : nums) {
+            if(entire.contains(i)) {
+                duplicated.add(i);
+            } else {
+                entire.add(i);
+            }
+        }
+        
+        entire.removeAll(duplicated);
+        for(Integer ret : entire) return ret;
+        return 0;
+    }
+}
