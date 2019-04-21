f0dc802942a3b3a68bb744751b593397f5742d40
Yoseob Kim <yo1226@gmail.com>
2019-04-21 23:40:17 +0900

    Create 143_Reorder_List_YS_KIM_2019_04_21.java

---
+++ 140/143_Reorder_List_YS_KIM_2019_04_21.java
@@ -0,0 +1,57 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    private ListNode jump(ListNode head, int jumpForLast) {
+        ListNode ret = head;
+        for(int i = 0; i < jumpForLast; i++) {
+            ret = ret.next;
+        }
+        
+        return ret;
+    }
+    
+    public void reorderList(ListNode head) {
+        if(head == null) return;
+        ListNode node = head;
+        
+        int jumpForLast = 0;
+        while(node.next != null) {
+            node = node.next;
+            jumpForLast++;
+        }
+        if(jumpForLast <= 1) return;
+            
+        ListNode first = head;
+        ListNode last = node;
+        ListNode prevLast = null;
+        int loopCount = 0;
+        int halfLen = (jumpForLast + 1) / 2;
+        while(true) {
+            if(first == last) {
+                prevLast.next = first;
+                first.next = null;
+                break;
+            }
+            if(loopCount >= halfLen) {
+                prevLast.next = null;
+                break;
+            }
+            
+            ListNode next = first.next;
+            first.next = last;
+            if(prevLast != null) prevLast.next = first;
+            
+            first = next;
+            prevLast = last;
+            jumpForLast = jumpForLast - 2;
+            last = jump(next, jumpForLast);
+            loopCount++;
+        }
+    }
+}
