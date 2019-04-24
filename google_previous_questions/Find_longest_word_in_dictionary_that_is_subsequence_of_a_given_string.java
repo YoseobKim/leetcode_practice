27b5759b467b7fda99611b033a97aa5530c61a75
Yoseob Kim <yo1226@gmail.com>
2019-04-24 20:45:46 +0900

    Update Find_longest_word_in_dictionary_that_is_subsequence_of_a_given_string.java

--- google_previous_questions/Find_longest_word_in_dictionary_that_is_subsequence_of_a_given_string.java
+++ google_previous_questions/Find_longest_word_in_dictionary_that_is_subsequence_of_a_given_string.java
@@ -1,7 +1,12 @@
+import java.util.*;
+
 public class HelloWorld{
+    private static Set<Character> sCharMap = new HashSet<>();
+    
     private static boolean canBeSubSequence(String S, String W) {
         boolean ret = false;
         if(S.length() < W.length()) return ret;
+        if(S.length() == W.length() && S.equals(W)) return true;
         int wIdx = 0;
         
         for(int i = 0; i < S.length(); i++) {
@@ -11,6 +16,7 @@ public class HelloWorld{
             }
             char sChar = S.charAt(i);
             char wChar = W.charAt(wIdx);
+            if(!sCharMap.contains(wChar)) break;
             if(sChar == wChar) wIdx++;
         }
         
@@ -30,6 +36,11 @@ public class HelloWorld{
     public static void main(String []args){
         String S = "abppplee";
         String[] D =  {"able", "ale", "apple", "bale", "kangaroo"};
+        
+        for(int i = 0; i < S.length(); i++) {
+            Character c = S.charAt(i);
+            sCharMap.add(c);
+        }
         
         System.out.println(longestSubSequence(S, D));
     }
