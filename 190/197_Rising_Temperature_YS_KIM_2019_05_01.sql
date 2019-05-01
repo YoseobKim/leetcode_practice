8cba16a177e29c4c510837d032a89f28109b80dc
Yoseob Kim <yo1226@gmail.com>
2019-05-01 20:51:10 +0900

    Create 197_Rising_Temperature_YS_KIM_2019_05_01.sql

---
+++ 190/197_Rising_Temperature_YS_KIM_2019_05_01.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+SELECT
+    Weather.Id AS 'Id'
+FROM
+    Weather
+        JOIN
+    Weather w ON DATEDIFF(Weather.RecordDate, w.RecordDate) = 1
+        AND Weather.Temperature > w.Temperature;
