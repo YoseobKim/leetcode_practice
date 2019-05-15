c92803c26948610751fd47f8454756b0e4cd6720
Yoseob Kim <yo1226@gmail.com>
2019-05-15 23:11:26 +0900

    Create 279_Perfect_Squares_YS_KIM_2019_05_15.java

---
+++ 270/279_Perfect_Squares_YS_KIM_2019_05_15.java
@@ -0,0 +1,27 @@
+class Solution {
+    private Map<Integer, Boolean> squareMap = new HashMap<>();
+    private Map<Integer, Integer> memo = new HashMap<>();
+    
+    private boolean isSquare(int n) {
+        if(squareMap.containsKey(n)) return squareMap.get(n);
+        int i = (int)Math.sqrt(n);
+        int ret = i * i;
+        squareMap.put(n, (ret == n));
+        return ret == n;
+    }
+    
+    public int numSquares(int n) {
+        int ret = n;
+        if(n <= 0) return 0;
+        if(isSquare(n)) return 1;
+        if(memo.containsKey(n)) return memo.get(n);
+        for(int i = 1; i < n; i++) {
+            if(isSquare(i)) {
+                int target = n - i;
+                ret = Math.min(ret, 1 + numSquares(target));
+            }
+        }
+        memo.put(n, ret);
+        return ret;
+    }
+}
