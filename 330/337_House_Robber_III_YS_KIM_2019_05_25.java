4cd2220d997e9306ed24b4c355aaf8bcc3a7e3eb
Yoseob Kim <yo1226@gmail.com>
2019-05-25 23:39:04 +0900

    Update 337_House_Robber_III_YS_KIM_2019_05_25.java

--- 330/337_House_Robber_III_YS_KIM_2019_05_25.java
+++ 330/337_House_Robber_III_YS_KIM_2019_05_25.java
@@ -8,8 +8,11 @@
  * }
  */
 class Solution {
+    private Map<TreeNode, Integer> memo = new HashMap<>();
     public int rob(TreeNode root) {
         if (root == null) return 0;
+        if(memo.containsKey(root)) return memo.get(root);
+        int ret = 0;
         int withMe = 0;
         if (root.left != null) {
             withMe += rob(root.left.left) + rob(root.left.right);
@@ -18,8 +21,9 @@ class Solution {
         if (root.right != null) {
             withMe += rob(root.right.left) + rob(root.right.right);
         }
-    
+        ret = Math.max(withMe + root.val, rob(root.left) + rob(root.right));
+        memo.put(root, ret);
         // compare with me and without me
-        return Math.max(withMe + root.val, rob(root.left) + rob(root.right));
+        return ret;
     }
 }
