6684f336e653eec1495e37846ba5bec89dd1c588
Yoseob Kim <yo1226@gmail.com>
2019-05-28 21:17:41 +0900

    Update 367_Valid_Perfect_Square_YS_KIM_2019_05_28.java

--- 360/367_Valid_Perfect_Square_YS_KIM_2019_05_28.java
+++ 360/367_Valid_Perfect_Square_YS_KIM_2019_05_28.java
@@ -1,7 +1,16 @@
 class Solution {
+   private int sqrt(int num) {
+        int ret = 0;
+        for(long i = 1; i * i <= num; i++) {
+            ret = (int)i;
+        }
+        return ret;
+    }
+    
     public boolean isPerfectSquare(int num) {
         if(num == 0) return false;
-        int sqrt = (int)Math.sqrt(num);
+        //int sqrt = (int)Math.sqrt(num);
+        int sqrt = sqrt(num);
         return sqrt * sqrt == num;
     }
 }
